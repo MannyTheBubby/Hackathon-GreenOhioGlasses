a18f0066aa350974a3b1bc990b72fe7a611dbdf8
Ch3ckmatedotsys <jjg7960@gmail.com>
2025-03-08 14:41:38 -0500

    updated

--- cli.py
+++ cli.py
@@ -1,39 +1,41 @@
-import socket 
-s = socket.socket()
-host = input(str("Please eenter the host address of the sender: "))
-port = 8080
-s.connect((host,port))
-print("Connected!")
-
-
-filename = input(str("Please enter the filename of the file you with to receive: "))
-file = open(filename, 'wb')
-file_data = s.recv(1024)
-file.write(file_data)
-file.close
-print("File Transfer Successful!!")
-
-
-# ---------------------------------------------------------------
-
-
 import socket
-import os
 from Crypto.Cipher import AES
 import hashlib
 
-s = socket.socket ()
-host = socket.gethostname ()
+BUFFER_SIZE = 4096 
+KEY = b'secure_key_16_bytes'
+
+def encrypt_file(data, key):
+    nonce = data[:16]
+    cipher = AES.new(hashlib.sha256(key).digest(), AES.MODE_EAX, nonce=nonce)
+    return cipher.encrypt(data[16:])
+
+s = socket.socket()
+host = socket.gethostname()
 port = 8080
-s.bind ((host,port))
+
+s.bind((host, port))
 s.listen()
 print(host)
-print("Waiting for incoming traffic. . . ")
-conn, addr = s.accept()
-print(addr, "Has connected to srvr")
 
-filename = input(str("Please enter a file you wish to transfer: ")) 
-file = open(filename, 'rb')
-file_data = file.read(1024)
-conn.send(file_data)
-print("Data has been transferred!")
+print("Waiting for incoming traffic... ")
+conn, addr = s.accept()
+print(addr, "Has connected to server")
+
+filename = input("Please enter a file you wish to transfer: ")
+
+try:
+    with open(filename, 'rb') as file:
+        while True:
+            file_data = file.read(BUFFER_SIZE)
+            if not file_data:
+                break
+            encrypted_data = encrypt_file(file_data, KEY)
+            conn.send(encrypted_data)
+            print("Sending data...")
+
+    print("File Transfer Successful!!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    s.close()
