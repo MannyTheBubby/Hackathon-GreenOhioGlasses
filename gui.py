9772030504dd6d25fa80315badf19eb9e0657662
Tanish <chaibot10000@gmail.com>
2025-03-08 16:01:30 -0500

    Gui update

--- gui.py
+++ gui.py
@@ -1,16 +1,44 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 
+window = None
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Acc Window")
         self.setGeometry(100, 100, 400, 300)
-        layout = QVBoxLayout()
+
+        # masterLayout = QHBoxLayout()
+
+        mainWin = QVBoxLayout()
+        actionSection = QHBoxLayout()
+
+        sendButton = QPushButton("Send")
+        sendButton.clicked.connect(self.send)
+
+        ReciveButton = QPushButton("Recive")
+        ReciveButton.clicked.connect(self.recive)
+
+        
+        
         label = QLabel("Hello, World!")
+        label1 = QLabel("Hello, World!")
+
+        layout = QVBoxLayout()
+
         layout.addWidget(label)
-        self.setLayout(layout)
+        layout.addWidget(label1)
+        layout.addStretch(100)
+
+        mainWin.addLayout(actionSection)
+        self.setLayout(mainWin)
+
+    def send(self):
+        None
+    
+    def recive(self):
+        None
 
 class LoginWindow(QWidget):
     def __init__(self):
@@ -18,19 +46,36 @@ class LoginWindow(QWidget):
         self.setWindowTitle("Login")
         self.setGeometry(100, 100, 600, 400)
 
-        layout = QHBoxLayout()
-        label = QLabel("Login")
-        layout.addWidget(label)
-        self.setLayout(layout)
+        dlgLayout = QVBoxLayout()
+        
+        layout = QFormLayout()
+        layout.setVerticalSpacing(20)
+        layout.addRow("Username", QLineEdit())
+        layout.addRow("Password", QLineEdit())
+
+        button = QPushButton("Login")
+        button.clicked.connect(self.login)
+        # button1 = QPushButton("signup")
+        # button1.clicked.connect(self.signUp)
+        
+        dlgLayout.addLayout(layout)
+        dlgLayout.addWidget(button)
+        self.setLayout(dlgLayout)
+
+    def login(self):
+        None
+
+    def signUP(self):
+        None
 
 
 def main():
     app = QApplication([])
     window = MainWindow()
-    # window.show()
+    window.show()
 
     loginWindow = LoginWindow()
-    loginWindow.show()
+    # loginWindow.show()
     
     
     app.exec()
