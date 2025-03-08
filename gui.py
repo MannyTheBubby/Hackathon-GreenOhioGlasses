f8a6147d60a6a607baf1a3032e7f6ce76cbd1193
Tanish <chaibot10000@gmail.com>
2025-03-08 13:23:28 -0500

    Updated GUI

--- gui.py
+++ gui.py
@@ -1,12 +1,36 @@
 from PyQt6.QtWidgets import *
 
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Acc Window")
+        self.setGeometry(100, 100, 800, 600)
+        layout = QVBoxLayout()
+        label = QLabel("Hello, World!")
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Login")
+        self.setGeometry(100, 100, 800, 600)
+        layout = QVBoxLayout()
+        label = QLabel("Hello, World!")
+        layout.addWidget(label)
+        self.setLayout(layout)
+
 def main():
     app = QApplication([])
-    window = QWidget()
+    window = MainWindow()
 
     label = QLabel(window)
     label.setText("Hello, World!")
     # label.setFont('Arial', 20)
+
+    loginWindow = LoginWindow()
+    loginWindow.show()
     
     window.show()
     app.exec()
